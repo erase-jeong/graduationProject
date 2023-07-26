2ba60aa7b215ca7b8c0cf911123d4cd7969f1b58
erase-jeong <erase.jeong@gmail.com>
2023-07-26 15:19:47 +0900

    soundGeneraotor code

--- soundGenerator.py
+++ soundGenerator.py
@@ -1,24 +1,33 @@
+import wave
 import pydub
 from pydub import AudioSegment
 from pydub.playback import play
+#from pydub import Audiosegment
 
-# 사운드 파일 불러오기
-sound = AudioSegment.from_file("phonecert.wav", format="wav")
+# beep 파일 불러오기(beep wav 파일)
+beep = AudioSegment.from_file("beep.wav", format="wav")
 
-# 좌우 채널 분리
-left_channel = sound.split_to_mono()[0]
-right_channel = sound.split_to_mono()[1]
-
-#사용자가 원하는 왼쪽/오른쪽 값 입력받기
-user_left_sound=input("user_left_sound : ")
-user_right_sound=input("user_right_sound : ")
+# noSound 파일 불러오기(공백 wav 파일)
+noSound = AudioSegment.from_file("noSound.wav", format="wav")
 
 
-left_channel=left_channel+user_left_sound
-right_channel=right_channel+user_right_sound
+values=AudioSegment.empty()
 
-#합치기
-adjusted_sound = AudioSegment.from_mono_audiosegments(left_channel, right_channel)
+def soundPeriod(input_time,cnt):
+    global values     #values 변수를 전역 변수로 선언
 
-#출력
-play(adjusted_sound)
+    userTime=input_time*1000   #1초 = 1000, 60초 = 60000초
+    period_length=float(userTime/cnt)
+    period_length2=float(period_length/2)
+    file=beep[0:period_length2]+noSound[0:period_length2]
+
+    for _ in range(cnt):
+        play(file)
+        values=values+file
+
+    # WAV 파일로 출력
+    output_file = "output_wav2.wav"
+    values.export(output_file, format="wav")
+    print("WAV 파일로 출력되었습니다:", output_file)
+
+#soundPeriod(1,5) #1초, 5회(주기 5)
